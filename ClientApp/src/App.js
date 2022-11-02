e6af256231651334a5bf76406811945f12c9b5b5
duy-nhat-1 <duyisdenhat@gmail.com>
2022-11-02 21:39:52 +0700

    setup_project

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,22 +1,10 @@
-import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
-import { Layout } from './components/Layout';
-import './custom.css';
-
-export default class App extends Component {
-  static displayName = App.name;
-
-  render() {
-    return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      </Layout>
-    );
-  }
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      adawdadadw
+    </div>
+  );
 }
+
+export default App;
